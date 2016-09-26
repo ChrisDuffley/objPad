dd17dce2446bfa093c23d40a1a04acd797c6c4fb
Joseph Lee <joseph.lee22590@gmail.com>
2016-09-25 21:29:41 -0700

    Experimental scan mode emulation.
    With this, up and down arrows will move by object, left and right arrows will read text.

--- addon/globalPlugins/objPad.py
+++ addon/globalPlugins/objPad.py
@@ -11,34 +11,51 @@ from globalCommands import commands
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
-	objArrowMode = False
+	objArrowMode = 0
 
 	def script_toggleObjArrows(self, gesture):
-		if not self.objArrowMode:
-			self.objArrowMode = True
-			self.bindGesture("kb:rightarrow", "objNext")
-			self.bindGesture("kb:leftarrow", "objPrev")
-			self.bindGesture("kb:downarrow", "objFirstChild")
-			self.bindGesture("kb:uparrow", "objParent")
-			self.bindGesture("kb:space", "objActivate")
+		if self.objArrowMode == 0:
+			self.objArrowMode = 1
 			ui.message("Arrows will move between objects")
+		elif self.objArrowMode == 1:
+			self.objArrowMode = 2
+			ui.message("Scan mode")
 		else:
-			self.objArrowMode = False
+			self.objArrowMode = 0
+			ui.message("Traditional object navigation mode")
+		if 0 < self.objArrowMode <= 2:
+			self.bindGesture("kb:rightarrow", "rightArrow")
+			self.bindGesture("kb:leftarrow", "leftArrow")
+			self.bindGesture("kb:downarrow", "downArrow")
+			self.bindGesture("kb:uparrow", "upArrow")
+			self.bindGesture("kb:space", "objActivate")
+		else:
 			self.clearGestureBindings()
 			self.bindGestures(self.__gestures)
-			ui.message("Traditional object navigation mode")
 
-	def script_objNext(self, gesture):
-		commands.script_navigatorObject_next(gesture)
+	def script_rightArrow(self, gesture):
+		if self.objArrowMode == 1:
+			commands.script_navigatorObject_next(gesture)
+		elif self.objArrowMode == 2:
+			commands.script_review_nextCharacter(gesture)
 
-	def script_objPrev(self, gesture):
-		commands.script_navigatorObject_previous(gesture)
+	def script_leftArrow(self, gesture):
+		if self.objArrowMode == 1:
+			commands.script_navigatorObject_previous(gesture)
+		elif self.objArrowMode == 2:
+			commands.script_review_previousCharacter(gesture)
 
-	def script_objFirstChild(self, gesture):
-		commands.script_navigatorObject_firstChild(gesture)
+	def script_downArrow(self, gesture):
+		if self.objArrowMode == 1:
+			commands.script_navigatorObject_firstChild(gesture)
+		elif self.objArrowMode == 2:
+			commands.script_navigatorObject_nextInFlow(gesture)
 
-	def script_objParent(self, gesture):
-		commands.script_navigatorObject_parent(gesture)
+	def script_upArrow(self, gesture):
+		if self.objArrowMode == 1:
+			commands.script_navigatorObject_parent(gesture)
+		elif self.objArrowMode == 2:
+			commands.script_navigatorObject_previousInFlow(gesture)
 
 	def script_objActivate(self, gesture):
 		commands.script_review_activate(gesture)
