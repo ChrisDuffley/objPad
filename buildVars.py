b29ae362e74538a6f9996b6211abfff76fb8992e
Joseph Lee <joseph.lee22590@gmail.com>
2019-12-20 11:53:07 -0800

    Manifest: add update channel flag

--- buildVars.py
+++ buildVars.py
@@ -30,6 +30,8 @@ addon_info = {
 	"addon_minimumNVDAVersion" : "2017.3.0",
 	# Last NVDA version supported/tested
 	"addon_lastTestedNVDAVersion" : "2019.1.0",
+	# Add-on update channel (default is stable)
+	"addon_updateChannel" : None,
 }
 
 
