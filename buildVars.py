d95ebad151b6afaa4d9c56ad51f2f021e074913f
Joseph Lee <joseph.lee22590@gmail.com>
2021-04-01 11:12:52 -0700

    Minimum NVDA version: 2020.1, last tested: 2021.1

--- buildVars.py
+++ buildVars.py
@@ -27,9 +27,9 @@ addon_info = {
 	# File name for the add-on help file.
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported
-	"addon_minimumNVDAVersion" : "2019.3.0",
+	"addon_minimumNVDAVersion" : "2020.1",
 	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion" : "2019.3.0",
+	"addon_lastTestedNVDAVersion" : "2021.1",
 	# Add-on update channel (default is stable)
 	"addon_updateChannel" : None,
 }
