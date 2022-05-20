85ffbf26cb578c77914fa618299b2dbfb4cd4c03
Joseph Lee <joseph.lee22590@gmail.com>
2022-05-20 14:08:14 -0700

    Add-on template: use 2022.1 version and include lint fixes in build vars

--- buildVars.py
+++ buildVars.py
@@ -3,42 +3,54 @@
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-# Full getext (please don't change)
-_ = lambda x : x
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# which returns whatever is given to it as an argument.
+def _(arg):
+	return arg
+
 
 # Add-on information variables
 addon_info = {
-	# for previously unpublished addons, please follow the community guidelines at:
-	# https://bitbucket.org/nvdaaddonteam/todo/src/56140dbec531e4d7591338e1dbc6192f3dd422a8/guideLines.txt
-	# add-on Name, internal for nvda
-	"addon_name" : "objPad",
+	# add-on Name/identifier, internal for NVDA
+	"addon_name": "objPad",
 	# Add-on summary, usually the user visible name of the addon.
-	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("ObjPad"),
+	# Translators: Summary for this add-on
+	# to be shown on installation and add-on information.
+	"addon_summary": _("ObjPad"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Perform various object commands by pressing a single key, including navigation and more"""),
+	"addon_description": _("""Perform various object commands by pressing a single key, including navigation and more"""),
 	# version
-	"addon_version" : "21.06",
+	"addon_version": "21.06",
 	# Author(s)
-	"addon_author" : "Joseph Lee <joseph.lee22590@gmail.com>",
+	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url" : "https://addons.nvda-project.org",
-	# File name for the add-on help file.
-	"addon_docFileName" : "readme.html",
-	# Minimum NVDA version supported
-	"addon_minimumNVDAVersion" : "2020.1",
-	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion" : "2021.1",
-	# Add-on update channel (default is stable)
-	"addon_updateChannel" : None,
+	"addon_url": "https://addons.nvda-project.org/",
+	# Documentation file name
+	"addon_docFileName": "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	"addon_minimumNVDAVersion": "2020.1",
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion": "2021.1",
+	# Add-on update channel (default is None, denoting stable releases,
+	# and for development releases, use "dev".)
+	# Do not change unless you know what you are doing!
+	"addon_updateChannel": None,
 }
 
 
-import os.path
-
 # Define the python files that are the sources of your add-on.
-# You can use glob expressions here, they will be expanded.
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+# pythonSources = ["addon/globalPlugins/*.py"]
+# For more information on SCons Glob expressions please take a look at:
+# https://scons.org/doc/production/HTML/scons-user/apd.html
 pythonSources = [os.path.join("addon", "globalPlugins", "objPad.py")]
 
 # Files that contain strings for translation. Usually your python sources
@@ -47,3 +59,15 @@ i18nSources = pythonSources + ["buildVars.py"]
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
 excludedFiles = []
+
+# Base language for the NVDA add-on
+# If your add-on is written in a language other than english, modify this variable.
+# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
+baseLanguage = "en"
+
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions = []
