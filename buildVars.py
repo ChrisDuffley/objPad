607c924cf33a76801e13dbb6e670452ce1153d33
Christopher Duffley <christophernduffley@gmail.com>
2024-04-02 12:36:34 -0400

    BuildVars: version = 24.04. Last tested NVDA Version -> 2024.1
    Signed-off-by: Christopher Duffley <christophernduffley@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Perform various object commands by pressing a single key, including navigation and more"""),
 	# version
-	"addon_version": "23.06",
+	"addon_version": "24.04",
 	# Author(s)
 	"addon_author": "Christopher Duffley <nvda@chrisduffley.com>, originally by Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2022.4",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.1",
+	"addon_lastTestedNVDAVersion": "2024.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
