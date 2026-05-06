709b7f9fa6b9f23525fc012e717ad5381401a659
Joseph Lee <joseph.lee22590@gmail.com>
2026-05-05 20:45:49 -0600

    BuildVars/changelog: replace 26.05 entry with 26.06 entry

--- buildVars.py
+++ buildVars.py
@@ -26,7 +26,7 @@ addon_info = AddonInfo(
 	addon_version="26.05",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""* NVDA 2025.3.3 or later is required."""),
+	addon_changelog=_("""* Reordered browse mode elements order to follow NVDA 2026.2 order."""),
 	# Author(s)
 	addon_author="Christopher Duffley <nvda@chrisduffley.com>, originally by Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
